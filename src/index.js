ddf3e595a454fae46464cdfe2f4c69dd6370ece5
Sachin Kumawat <sachin09688@gmail.com>
2025-06-18 09:55:38 +0530

    commit-two

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import Home from './section/Home/Home';
 import About from './section/About/About';
 import {
-   createBrowserRouter,
-   RouterProvider,
- } from "react-router-dom";
- import reportWebVitals from './reportWebVitals';
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
+import reportWebVitals from './reportWebVitals';
 import Product from './section/products/Product';
 import Contact from './section/Contact/Contact';
 import Leadership from './section/Leadership/Leadership';
@@ -19,62 +19,23 @@ import TandC from './section/Footer/Policy/TandC';
 import PrivacyPolicy from './section/Footer/Policy/PrivacyPolicy';
 import Disclaimer from './section/Footer/Policy/Disclaimer';
 
+const router = createHashRouter([
+  { path: "/", element: <Home /> },
+  { path: "/about", element: <About /> },
+  { path: "/leadership", element: <Leadership /> },
+  { path: "/career", element: <Career /> },
+  { path: "/contact", element: <Contact /> },
+  { path: "/product", element: <Product /> },
+  { path: "/life-at-sawariya", element: <LifeAtSawariya /> },
+  { path: "/training", element: <Training /> },
+  { path: "/current", element: <Opportunities /> },
+  { path: "/terms-conditions", element: <TandC /> },
+  { path: "/privacy--policy", element: <PrivacyPolicy /> },
+  { path: "/disclaimer", element: <Disclaimer /> },
+]);
 
- const router = createBrowserRouter([
-   {
-     path: "/",
-     element: <Home/>,
-   },
-   {
-      path: "/about",
-      element: <About/>,
-    },
-    {
-      path: "/leadership",
-      element: <Leadership/>,
-    },
-    {
-      path: "/career",
-      element: <Career/>,
-    },
-    {
-      path: "/contact",
-      element: <Contact/>,
-    },{
-      path: "/product",
-      element: <Product/>,
-    },
-    {
-      path: "/life-at-sawariya",
-      element: <LifeAtSawariya/>,
-    },
-    {
-      path: "/training",
-      element: <Training/>,
-    },
-    {
-      path: "/current",
-      element: <Opportunities/>,
-    },
-     {
-      path: "/terms-conditions",
-      element: <TandC/>,
-    },
-    {
-      path: "/privacy--policy",
-      element: <PrivacyPolicy/>,
-    },
-    {
-      path: "/disclaimer",
-      element: <Disclaimer/>
-    },
- ]);
-
- ReactDOM.createRoot(document.getElementById("root")).render(
- 
-     <RouterProvider router={router} />
-     
-  
- );
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
 
 reportWebVitals();
